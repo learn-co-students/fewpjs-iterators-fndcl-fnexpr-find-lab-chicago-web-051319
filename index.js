11be079623b8d0ff33fa53cc3b29e2b7f0ca20f1
afhare <alexandra.f.hare@gmail.com>
2019-07-01 10:54:19 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(record){
+  const winningYear = record.find(function(recordObject){
+    if (recordObject.result === "W"){
+      return recordObject
+    }
+  });
+  if (winningYear){
+   return winningYear.year;
+  }
+  return winningYear;
+};
